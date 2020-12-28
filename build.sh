906e2c298751918326bf75185f967838099ca6cd
Mcdull <1042762344@qq.com>
2020-12-29 00:35:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/geph-official/geph-android.git
 sed -i "s/io.geph.android/io.geph.android367/g" `grep "io.geph.android" -rl geph-android`
+sed -i "s/io_geph_android/io_geph_android367/g" `grep "io_geph_android" -rl geph-android`
 mv geph-android/app/src/main/java/io/geph/android geph-android/app/src/main/java/io/geph/android367
 cd geph-android
 bash UPDATE-DAEMON.sh
