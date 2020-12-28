37b143b0f73bece3de753827649d91fe63d89aac
Mcdull <1042762344@qq.com>
2020-12-28 23:43:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ git clone https://github.com/geph-official/geph-android.git
 cd geph-android
 bash UPDATE-DAEMON.sh
 chmod 755 gradlew
-sed -i "s/io.geph.android/io.geph.android367/g" app/src/main/AndroidManifest.xml
+#sed -i "s/io.geph.android/io.geph.android367/g" app/src/main/AndroidManifest.xml
 ./gradlew assemblereleaseAPK
 sudo apt install tree 
 tree
