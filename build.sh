6f71cbc8923122bcff7f8ea50ff6e36dd1fb3de3
Mcdull <1042762344@qq.com>
2020-12-28 23:17:41 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/geph-official/geph-android.git
+cd geph-android
+bash UPDATE-DAEMON.sh
+chmod 755 gradlew
+sed -i "s/io.geph.android/io.geph.android367/g" app/src/main/AndroidManifest.xml
+./gradlew help
