95d49fb67402224efbd776ddc43be74f1a0f9e10
Mcdull <1042762344@qq.com>
2020-12-28 23:31:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ cd geph-android
 bash UPDATE-DAEMON.sh
 chmod 755 gradlew
 sed -i "s/io.geph.android/io.geph.android367/g" app/src/main/AndroidManifest.xml
-./gradlew tasks
-./gradlew task
+./gradlew assemblereleaseAPK
+apt install tree 
+tree
